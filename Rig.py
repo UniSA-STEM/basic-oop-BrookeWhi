a7e5a8ae57ede2775b313fb177f707a6fd5ae77a
Brooke Whitmore <whiby031@mymail.unisa.edu.au>
2025-10-14 21:46:00 +1100

    Update constructor details, Create Asset Generation function

--- Rig.py
+++ Rig.py
@@ -6,13 +6,27 @@ ID: 110468647
 Username: BrookeWhi
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+import random
 
 class Rig:
-    def __init__(self, name, damage_counter, broken_state, storage, upgrade_level, damage_max):
-        self.name = name
-        self.damage_counter = 0
-        self.broken_state = "Pristine"
-        self.storage = ["Data Spike", "Data Spike", "Removable Drive"]
-        self.upgrade_level = 0
-        self.damage_max = 2
+    def __init__(self, name):
+        self.__name = name
+        self.__damage_counter = 0
+        self.__broken_state = "Pristine"
+        self.__storage = [("Data Spike", "Used in battles."), ("Data Spike", "Used in battles."), ("Removable Drive", "Found in rigs and used for extraction.")]
+        self.__upgrade_level = 0
+        self.__damage_max = 2
 
+    def assetGen(self):
+        assets = ["CryptoToken", "Data Spike", "Removable Drive", "Security Chip", "Hardware Patch"]
+        asset_desc = ["Used to acquire or repair rigs.", "Used in battles.", "Found in rigs and used for extraction.",
+                      "Used to encrypt or decrypt.", "Used to upgrade rigs."]
+        index = random.randrange(len(assets))
+        new_asset = [(assets[index], asset_desc[index])]
+        self.__storage.append(new_asset)
+
+### Testing
+# x = Rig("PC")
+# print(x._Rig__storage)
+# x.assetGen()
+# print(x._Rig__storage)
