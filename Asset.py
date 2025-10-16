fadf17405a7165c881e0666a35e6f49c1fb0c8ea
Brooke Whitmore <whiby031@mymail.unisa.edu.au>
2025-10-16 20:23:07 +1100

    Added Functions
    1. encrypt and decrypt functions added
    2. added name and description getters

--- Asset.py
+++ Asset.py
@@ -18,3 +18,35 @@ class Asset:
             return (f"Asset: {self.__name}: {self.__description} [Encrypted]")
         else:
             return (f"Asset: {self.__name}: {self.__description}")
+
+    def encrypt(self):
+        print(self)
+        if self.__encrypted == False:
+            self.__encrypted = True
+            print(f"{self.__name} now encrypted")
+        else:
+            print(f"{self.__name} already encrypted")
+
+    def decrypt(self):
+        if self.__encrypted == True:
+            self.__encrypted = False
+            print(f"{self.__name} now decrypted")
+        else:
+            print(f"{self.__name} cannot be decrypted")
+
+
+
+    def get_name(self):
+        return self.__name
+
+    def get_description(self):
+        return self.__description
+
+    # def assets(self):
+    #     return {
+    #         "CryptoToken": Asset("CryptoToken", "Used to acquire or repair rigs."),
+    #         "Data Spike": Asset("Data Spike", "Used in battles."),
+    #         "Removable Drive": Asset("Removable Drive", "Found in rigs and used for extraction."),
+    #         "Security Chip": Asset("Security Chip", "Used to encrypt or decrypt."),
+    #         "Hardware Patch": Asset("Hardware Patch", "Used to upgrade rigs.")
+    #     }
